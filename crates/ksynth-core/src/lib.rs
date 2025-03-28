53b3b6aae3e6a13b67a048a026e0c0587751c4e8
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2025-03-28 14:07:22 +0900

    Add WASM support

--- crates/ksynth-core/src/lib.rs
+++ crates/ksynth-core/src/lib.rs
@@ -4,8 +4,14 @@ pub mod voice;
 use std::{
     collections::{HashMap, VecDeque},
     sync::{Arc, Mutex},
-    time::{Duration, Instant},
+    time::Duration,
 };
+
+#[cfg(not(target_arch = "wasm32"))]
+use std::time::Instant;
+
+#[cfg(target_arch = "wasm32")]
+use web_time::Instant;
 
 use sample::{Sample, SampleData};
 use voice::Voice;
