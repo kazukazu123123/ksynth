28c52699cafd34253527402811a4b0a840a34d76
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2025-03-29 16:18:54 +0900

    Revert "Add WASM support"
    This reverts commit 53b3b6aae3e6a13b67a048a026e0c0587751c4e8.

--- crates/ksynth-core/src/lib.rs
+++ crates/ksynth-core/src/lib.rs
@@ -4,14 +4,8 @@ pub mod voice;
 use std::{
     collections::{HashMap, VecDeque},
     sync::{Arc, Mutex},
-    time::Duration,
+    time::{Duration, Instant},
 };
-
-#[cfg(not(target_arch = "wasm32"))]
-use std::time::Instant;
-
-#[cfg(target_arch = "wasm32")]
-use web_time::Instant;
 
 use sample::{Sample, SampleData};
 use voice::Voice;
