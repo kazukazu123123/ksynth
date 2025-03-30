3e6dde855f791130ee19c7089fcb712fd72ad1a0
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2025-03-30 22:19:45 +0900

    Fix `note_off` sometime doesn't note off

--- crates/ksynth-core/src/lib.rs
+++ crates/ksynth-core/src/lib.rs
@@ -316,7 +316,6 @@ impl KSynth {
         for voice in self.voices.iter_mut() {
             if voice.get_channel() == channel && voice.get_note() == note {
                 voice.set_is_releasing(true);
-                break;
             }
         }
     }
