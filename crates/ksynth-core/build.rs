b5a6f3c9c5398c4161321bb2bee621888d0c5c0d
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2025-04-12 11:30:22 +0900

    Run rustfmt

--- crates/ksynth-core/build.rs
+++ crates/ksynth-core/build.rs
@@ -1,7 +1,10 @@
 fn main() {
     match get_git_commit_hash_result() {
         Ok(result) => println!("cargo::rustc-env=KSYNTH_BUILD_GIT_COMMIT_HASH={result}"),
-        Err(_) => println!("cargo::rustc-env=KSYNTH_BUILD_GIT_COMMIT_HASH={}", "Failed to retrieve the commit hash."),
+        Err(_) => println!(
+            "cargo::rustc-env=KSYNTH_BUILD_GIT_COMMIT_HASH={}",
+            "Failed to retrieve the commit hash."
+        ),
     }
 }
 
