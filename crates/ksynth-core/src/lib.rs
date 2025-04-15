745d34c0d7446f05287ebc3b63823a9eb52ca094
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2025-04-15 18:13:32 +0900

    Changed the process of deleting old voices when the current number of voices has exceeded the maximum number of voices when note_on.

--- crates/ksynth-core/src/lib.rs
+++ crates/ksynth-core/src/lib.rs
@@ -460,17 +460,8 @@ impl KSynth {
         }
 
         if self.polyphony >= self.max_polyphony {
-            if let Some(quietest_voice_index) = self
-                .voices
-                .iter()
-                .enumerate()
-                .filter(|(_, v)| v.get_is_active())
-                .min_by_key(|(_, v)| v.get_velocity())
-                .map(|(index, _)| index)
-            {
-                self.voices.remove(quietest_voice_index);
-                self.polyphony -= 1;
-            }
+            self.voices.remove(0);
+            self.polyphony -= 1;
         }
 
         let voice = Voice::new(channel, note, velocity);
