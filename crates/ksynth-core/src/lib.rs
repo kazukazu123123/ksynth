f7755a3f7038bc08cf2757d68aa0f6759184e09d
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2025-04-18 14:02:18 +0900

    Revert "Change release curve" (Slow)
    This reverts commit 9463e714a061288d00b4a512513e14ace8f038e0.

--- crates/ksynth-core/src/lib.rs
+++ crates/ksynth-core/src/lib.rs
@@ -355,7 +355,8 @@ impl KSynth {
                             if samples_since_release >= fade_samples {
                                 voice.set_is_active(false);
                             } else {
-                                amplitude *= 1.0 - (samples_since_release as f32 / fade_samples as f32).powf(2.0);
+                                amplitude *=
+                                    1.0 - (samples_since_release as f32 / fade_samples as f32);
                             }
                         }
                     }
